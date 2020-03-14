ee1953a931e621d6ff961e9183702ac30359e438
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-03-14 15:29:22 -0500

    Add find methods in system state SITM

--- State/src/model/SystemStateProducte.java
+++ State/src/model/SystemStateProducte.java
@@ -1,18 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+
 import productFamilies.IBus;
 import productFamilies.IEstation;
 import productFamilies.IPlane;
 import productFamilies.IRoute;
 import productFamilies.IStop;
 
-public abstract class SystemStateProducte {
+public class SystemStateProducte {
 
-	private IBus bus;
-	private IPlane plane;
-	private IEstation estation;
-	private IRoute route;
-	private IStop stop;
+	protected ArrayList<IBus>buses;
+	protected ArrayList<IPlane> planes;
+	protected ArrayList<IEstation> estations;
+	protected ArrayList<IRoute> routes;
+	protected ArrayList<IStop> stops;
+	
+	public ArrayList<IBus> getBuses() {
+		return buses;
+	}
+	public void setBuses(ArrayList<IBus> buses) {
+		this.buses = buses;
+	}
+	public ArrayList<IPlane> getPlanes() {
+		return planes;
+	}
+	public void setPlanes(ArrayList<IPlane> planes) {
+		this.planes = planes;
+	}
+	public ArrayList<IEstation> getEstations() {
+		return estations;
+	}
+	public void setEstations(ArrayList<IEstation> estations) {
+		this.estations = estations;
+	}
+	public ArrayList<IRoute> getRoutes() {
+		return routes;
+	}
+	public void setRoutes(ArrayList<IRoute> routes) {
+		this.routes = routes;
+	}
+	public ArrayList<IStop> getStops() {
+		return stops;
+	}
+	public void setStops(ArrayList<IStop> stops) {
+		this.stops = stops;
+	}
 	
 	
 	
--- State/src/model/SystemStateSITM.java
+++ State/src/model/SystemStateSITM.java
@@ -1,5 +1,66 @@
 package model;
 
+import concreteSITM.BusSITM;
+import concreteSITM.EstationSITM;
+import concreteSITM.PlaneSITM;
+import concreteSITM.RouteSITM;
+import concreteSITM.StopSITM;
+import productFamilies.IBus;
+import productFamilies.IEstation;
+import productFamilies.IPlane;
+import productFamilies.IRoute;
+import productFamilies.IStop;
+
 public class SystemStateSITM extends SystemStateProducte {
 
+	private IBus findBusById(String id) {
+		
+		for (int i = 0; i < buses.size(); i++) {
+			BusSITM concreteBus = (BusSITM)buses.get(i);
+			if (concreteBus.getLicencePlate().equals(id)) {
+				return concreteBus;
+			}
+		}
+		return null;
+	}
+	
+	private IPlane findPlanesById(String id) {
+		for (int i = 0; i < planes.size(); i++) {
+			PlaneSITM concretePlane = (PlaneSITM)planes.get(i);
+			if (concretePlane.getId().equals(id)) {
+				return concretePlane;
+			}
+		}
+		return null;
+	}
+	
+	private IEstation findEstationsById(String id) {
+		for (int i = 0; i < estations.size(); i++) {
+			EstationSITM concreteEstation = (EstationSITM)estations.get(i);
+			if (concreteEstation.getName().equals(id)) {
+				return concreteEstation;
+			}
+		}
+		return null;
+	}
+	
+	private IRoute findRoutesById(String id) {
+		for (int i = 0; i < routes.size(); i++) {
+			RouteSITM concreteRoute = (RouteSITM)routes.get(i);
+			if (concreteRoute.getId().equals(id)) {
+				return concreteRoute;
+			}
+		}
+		return null;
+	}
+	
+	private IStop findStopsById(String id) {
+		for (int i = 0; i < stops.size(); i++) {
+			StopSITM concreteStop = (StopSITM)stops.get(i);
+			if (concreteStop.getId().equals(id)) {
+				return concreteStop;
+			}
+		}
+		return null;
+	}
 }
