6a2c79f2d174f29d0cae33d83131cbf8700b0f17
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-03-14 15:51:05 -0500

    Add declaration of update methods

--- State/src/model/SystemStateSITM.java
+++ State/src/model/SystemStateSITM.java
@@ -14,73 +14,124 @@ import productFamilies.IStop;
 public class SystemStateSITM extends SystemStateProducte {
 
 	private IBus findBusById(String id) {
-		
+
 		for (int i = 0; i < buses.size(); i++) {
-			BusSITM concreteBus = (BusSITM)buses.get(i);
+			BusSITM concreteBus = (BusSITM) buses.get(i);
 			if (concreteBus.getLicencePlate().equals(id)) {
 				return concreteBus;
 			}
 		}
 		return null;
 	}
-	
+
 	private IPlane findPlanesById(String id) {
 		for (int i = 0; i < planes.size(); i++) {
-			PlaneSITM concretePlane = (PlaneSITM)planes.get(i);
+			PlaneSITM concretePlane = (PlaneSITM) planes.get(i);
 			if (concretePlane.getId().equals(id)) {
 				return concretePlane;
 			}
 		}
 		return null;
 	}
-	
+
 	private IEstation findEstationsById(String id) {
 		for (int i = 0; i < estations.size(); i++) {
-			EstationSITM concreteEstation = (EstationSITM)estations.get(i);
+			EstationSITM concreteEstation = (EstationSITM) estations.get(i);
 			if (concreteEstation.getName().equals(id)) {
 				return concreteEstation;
 			}
 		}
 		return null;
 	}
-	
+
 	private IRoute findRoutesById(String id) {
 		for (int i = 0; i < routes.size(); i++) {
-			RouteSITM concreteRoute = (RouteSITM)routes.get(i);
+			RouteSITM concreteRoute = (RouteSITM) routes.get(i);
 			if (concreteRoute.getId().equals(id)) {
 				return concreteRoute;
 			}
 		}
 		return null;
 	}
-	
+
 	private IStop findStopsById(String id) {
 		for (int i = 0; i < stops.size(); i++) {
-			StopSITM concreteStop = (StopSITM)stops.get(i);
+			StopSITM concreteStop = (StopSITM) stops.get(i);
 			if (concreteStop.getId().equals(id)) {
 				return concreteStop;
 			}
 		}
 		return null;
 	}
-	
+
+	private void updateBusById(String id) {
+
+		for (int i = 0; i < buses.size(); i++) {
+			BusSITM concreteBus = (BusSITM) buses.get(i);
+			if (concreteBus.getLicencePlate().equals(id)) {
+				
+			}
+		}
+	}
+
+	private void updatePlanesById(String id) {
+		for (int i = 0; i < planes.size(); i++) {
+			PlaneSITM concretePlane = (PlaneSITM) planes.get(i);
+			if (concretePlane.getId().equals(id)) {
+
+			}
+		}
+
+	}
+
+	private void updateEstationsById(String id) {
+		for (int i = 0; i < estations.size(); i++) {
+			EstationSITM concreteEstation = (EstationSITM) estations.get(i);
+			if (concreteEstation.getName().equals(id)) {
+				
+			}
+		}
+		
+	}
+
+	private void updateRoutesById(String id) {
+		for (int i = 0; i < routes.size(); i++) {
+			RouteSITM concreteRoute = (RouteSITM) routes.get(i);
+			if (concreteRoute.getId().equals(id)) {
+				
+			}
+		}
+		
+	}
+
+	private void updateStopsById(String id) {
+		for (int i = 0; i < stops.size(); i++) {
+			StopSITM concreteStop = (StopSITM) stops.get(i);
+			if (concreteStop.getId().equals(id)) {
+				
+			}
+		}
+		
+	}
+
 	private void addBus(BusSITM bus) {
 		buses.add(bus);
 	}
-	
+
 	private void addEstation(EstationSITM estation) {
 		estations.add(estation);
 	}
-	
+
 	private void addPlane(PlaneSITM plane) {
 		planes.add(plane);
 	}
-	
+
 	private void addRoute(RouteSITM route) {
 		routes.add(route);
 	}
-	
+
 	private void addStop(StopSITM stop) {
 		stops.add(stop);
 	}
+
 }
